c78c61e568c8f9661a09897f66789513cdf59dfb
MartinD-CZ <martin@embedblog.eu>
2022-03-08 11:38:03 +0100

    added Release configuration

--- App/version.h
+++ App/version.h
@@ -1,12 +1,12 @@
 #ifndef VERSION_H_
 #define VERSION_H_
 
-#define _V_GIT_FULL       (const char*)"2.1.0.RC1-0-gca8cd88-dirty"
+#define _V_GIT_FULL       (const char*)"2.1.0.RC1-1-g2e75a48-dirty"
 #define _V_BUILD_TAG      (const char*)"2.1.0.RC1"
-#define _V_COMMIT         (const char*)"gca8cd88"
-#define _V_COMMITS_AHEAD   0
+#define _V_COMMIT         (const char*)"g2e75a48"
+#define _V_COMMITS_AHEAD   1
 #define _V_GIT_DIRTY_FLAG
-#define _V_DATE           (const char*)"08.03.2022 10:31"
+#define _V_DATE           (const char*)"08.03.2022 11:37"
 #define _V_COMPNAME       (const char*)"DELL-MARTIN"
 
 #endif
